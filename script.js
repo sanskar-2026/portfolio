5dda25f0105cc4ead3b5a6a05c5803323dc4bbe9
sanskar-2026 <sanskarmandal2006@gmail.com>
2026-04-20 10:43:10 +0530

    Add files via upload
    second commit

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// Smooth scroll
 function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({
         behavior: "smooth"
     });
 }
-
-// Initialize AOS animations
-AOS.init({
-    duration: 1000,
-});
